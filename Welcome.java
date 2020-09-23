c03213a8b264f3fdec87b3b0b7d7f024e136a329
unknown <ompreetam317@gmail.com>
2020-09-23 20:59:51 +0530

    [Preetam]Added: Welcome Message And UC1

---
+++ Welcome.java
@@ -0,0 +1,21 @@
+
+public class Welcome{
+	public static final int PLAYER1_START = 0;
+	public static final int PLAYER2_START = 0;
+	
+
+	public static void main(String[] args) {
+        	System.out.println("Welcome to Snake and ladder problem");
+		
+		
+		
+		
+	
+	
+	}
+
+
+
+
+
+}
