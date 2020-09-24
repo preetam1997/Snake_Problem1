5b41a4d1e32c6c113c40c0f6da3cf04624b0b14d
Preetam Mukhopadhyay <ompreetam317@gmail.com>
2020-09-24 15:18:37 +0530

    [Preetam]Refactored : UC3-MoveChoice

--- Welcome.java
+++ Welcome.java
@@ -1,4 +1,34 @@
 
+ 	public class Welcome{
+		public static final int PLAYER_START = 0;
+	
+	
+
+			public static void main(String[] args) {
+        			System.out.println("Welcome to Snake and ladder problem");
+		
+		
+		
+		
+	
+	
+			}
+
+
+		public static int roll_dice(){
+	
+			System.out.println("Rolling Dice ............");
+			double dice_num =1+ (Math.floor(Math.random()*10)%6);
+			System.out.println("You got " + dice_num);
+			return (int)dice_num;
+
+		}
+
+
+
+
+
+
 	public static int choice_num(){
 
 		System.out.println("Generating Choice ................");
